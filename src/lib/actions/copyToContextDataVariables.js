6629aa58064fd0a575784369ba2d7fda4eb766b9
Jan Exner <jexner@adobe.com>
2018-12-03 21:55:28 +0100

    in which our hero adds listeners

--- src/lib/actions/copyToContextDataVariables.js
+++ src/lib/actions/copyToContextDataVariables.js
@@ -22,78 +22,81 @@ const maxIndexProps = 75;
 const maxIndexEVars = 200;
 const maxIndexEvents = 1000;
 
+const augmentTracker = turbine.getSharedModule("adobe-analytics","augment-tracker");
+
 module.exports = function(settings) {
   var namespace = settings.namespace || "";
   if ("" !== namespace) {
     namespace = namespace + ".";
   }
   // recover tracker
-  const augmentTracker = turbine.getSharedModule("adobe-analytics","augment-tracker");
-  console.log(augmentTracker);
-
-  augmentTracker && augmentTracker( function(s) {
-    // copy built-in variables first
-    for (let i = 0; i < builtInVariableList.length; i++) {
-      const element = builtInVariableList[i];
-      if (weShouldCopy(element)) {
-        copy(s, namespace, element);
-      }
-    }
-
-    // copy built-in events
-    if (weShouldCopy('builtInEvents')) {
-      const events = s.events;
-      if ('undefined' !== events && events) {
-        for (let i = 0; i < builtInEventsList.length; i++) {
-          const event = builtInEventsList[i];
-          if (events.indexOf(event) >= 0) {
-            s.contextData[namespace + event] = event;
+  if (augmentTracker) {
+    augmentTracker( function(tracker) {
+      if (tracker) {
+        // copy built-in variables first
+        for (let i = 0; i < builtInVariableList.length; i++) {
+          const element = builtInVariableList[i];
+          if (weShouldCopy(element)) {
+            copy(tracker, namespace, element);
           }
         }
-      }
-    }
 
-    // copy eVars
-    if (weShouldCopy('eVars')) {
-      for (let i = 1; i <= maxIndexEVars; i++) {
-        const eVar = s['v' + i];
-        if ('undefined' !== typeof eVar && eVar) {
-          s.contextData[namespace + 'eVar' + i] = s[eVar];
-        }
-      }
-    }
-
-    // copy props
-    if (weShouldCopy('props')) {
-      for (let i = 1; i <= maxIndexProps; i++) {
-        const prop = s['c' + i];
-        if ('undefined' !== typeof prop && prop) {
-          s.contextData[namespace + 'prop' + i] = s[prop];
-        }
-      }
-    }
-
-    // copy listVars
-    if (weShouldCopy('listVars')) {
-      copy(s, namespace, 'list1');
-      copy(s, namespace, 'list2');
-      copy(s, namespace, 'list3');
-    }
-
-    // copy events
-    if (weShouldCopy('events')) {
-      const events = s.events;
-      if ('undefined' !== events && events) {
-        for (let i = 1; i <= maxIndexEvents; i++) {
-          const event = 'event' + i;
-          if (events.indexOf(event) >= 0) {
-            s.contextData[namespace + event] = event;
+        // copy built-in events
+        if (weShouldCopy('builtInEvents')) {
+          const events = s.events;
+          if ('undefined' !== events && events) {
+            for (let i = 0; i < builtInEventsList.length; i++) {
+              const event = builtInEventsList[i];
+              if (events.indexOf(event) >= 0) {
+                s.contextData[namespace + event] = event;
+              }
+            }
           }
         }
-      }
-    }
-  });
-};
+
+        // copy eVars
+        if (weShouldCopy('eVars')) {
+          for (let i = 1; i <= maxIndexEVars; i++) {
+            const eVar = s['v' + i];
+            if ('undefined' !== typeof eVar && eVar) {
+              s.contextData[namespace + 'eVar' + i] = s[eVar];
+            }
+          }
+        }
+
+        // copy props
+        if (weShouldCopy('props')) {
+          for (let i = 1; i <= maxIndexProps; i++) {
+            const prop = s['c' + i];
+            if ('undefined' !== typeof prop && prop) {
+              s.contextData[namespace + 'prop' + i] = s[prop];
+            }
+          }
+        }
+
+        // copy listVars
+        if (weShouldCopy('listVars')) {
+          copy(s, namespace, 'list1');
+          copy(s, namespace, 'list2');
+          copy(s, namespace, 'list3');
+        }
+
+        // copy events
+        if (weShouldCopy('events')) {
+          const events = s.events;
+          if ('undefined' !== events && events) {
+            for (let i = 1; i <= maxIndexEvents; i++) {
+              const event = 'event' + i;
+              if (events.indexOf(event) >= 0) {
+                s.contextData[namespace + event] = event;
+              }
+            }
+          }
+        }
+      } // if tracker
+  	});
+  }
+}
 
 function weShouldCopy(area) {
   return ('undefined' !== typeof settings[area] && true === settings[area]);
--- src/lib/actions/setContextDataVariables.js
+++ src/lib/actions/setContextDataVariables.js
@@ -1,5 +1,24 @@
 'use strict';
 
+// recover tracker
+const augmentTracker = turbine.getSharedModule("adobe-analytics","augment-tracker");
+
 module.exports = function(settings) {
-  // TODO Perform some action.
+  var namespace = settings.namespace || "";
+  if ("" !== namespace) {
+    namespace = namespace + ".";
+  }
+
+  if (augmentTracker) {
+    augmentTracker( function(tracker) {
+      if (tracker) {
+        // iterate through cd vars
+        for (var key in settings.contextData) {
+          var cdName = namespace + key;
+          var cdValue = settings.contextData[key];
+          tracker.contextData[cdName] = cdValue;
+        }
+      }
+    });
+  }
 };
