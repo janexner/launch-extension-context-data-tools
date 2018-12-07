29f74890ed92ad51bdc734ce88bb980d94e84f18
Jan Exner <jexner@adobe.com>
2018-12-07 21:20:01 +0100

    in which our hero fixes handling of data layers containing arrays

--- src/lib/actions/setContextDataFromDataLayer.js
+++ src/lib/actions/setContextDataFromDataLayer.js
@@ -21,13 +21,31 @@ function addElementsFromObjectToTrackerRecursively(root, prefix, tracker, joinCh
     if ('undefined' !== root && root) {
         if (Array.isArray(root)) {
             // join
-            tracker.contextData[prefix.join('.')] = root.join(joinCharacter);
+            var anyComplexTypesInHere = false;
+            for (var i = 0; i < root.length; i++) {
+                if ('object' === typeof root[i] || Array.isArray(root[i])) {
+                    anyComplexTypesInHere = true;
+                    break;
+                }
+            }
+            if (anyComplexTypesInHere) {
+                // TODO - we need to flatten the array here
+                for (var i = 0; i < root.length; i++) {
+                    var element = root[i];
+                    var newPrefix = Array.from(prefix);
+                    newPrefix.push(i);
+                    addElementsFromObjectToTrackerRecursively(element, newPrefix, tracker, joinCharacter);
+                }
+            } else {
+                tracker.contextData[prefix.join('.')] = root.join(joinCharacter);
+            }
         } else if ('object' === typeof root) {
             // drill down
             for (var key in root) {
                 if (root.hasOwnProperty(key)) {
-                    prefix.push(key);
-                    addElementsFromObjectToTrackerRecursively(root[key], prefix, tracker, joinCharacter);
+                	var newPrefix = Array.from(prefix);
+                	newPrefix.push(key);
+                    addElementsFromObjectToTrackerRecursively(root[key], newPrefix, tracker, joinCharacter);
                 }
             }
         } else {
